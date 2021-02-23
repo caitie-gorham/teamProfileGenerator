d2e0b991e87edd89574d54f6ff1d179ce2128368
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-02-22 21:04:37 -0600

    added framework for manager.test.js file

---
+++ tests/Manager.test.js
@@ -0,0 +1,20 @@
+// create variable to require the Employee.js file and the Manager.test.js file
+const Manager = require("../lib/Manager");
+const Employee = require("../lib/Employee");
+
+describe("Manager class", () => {
+    // test the creation of a new instance of the object - should have added property officeNumber
+    it("should do something", () => {
+        
+    });
+
+    // test getRole method - should be manager
+    describe("getRole", () => {
+        it("should do something", () => {
+            
+        });
+
+    });
+
+
+})
