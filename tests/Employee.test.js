66aef7426889c873520cc9a38c75ab9c1ba7b212
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-02-22 20:50:38 -0600

    set up framework for employee.test.js

---
+++ tests/Employee.test.js
@@ -0,0 +1,36 @@
+// create variable to require the Employee.js file
+const Employee = require('../lib/Employee')
+
+describe("Employee", () => {
+    // test getName method
+    describe("getName", () => {
+        it("should do something", () => {
+            
+        });
+
+    });
+
+    // test getID method
+    describe("getID", () => {
+        it("should do something", () => {
+            
+        });
+        
+    });
+
+    // test getEmail method
+    describe("getEmail", () => {
+        it("should do something", () => {
+            
+        });
+        
+    });
+
+    // test getRole method
+    describe("getRole", () => {
+        it("should do something", () => {
+            
+        });
+        
+    });
+});
