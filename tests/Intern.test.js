5129017ca8ceaa80883da187182605374c0a8673
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-02-22 21:02:57 -0600

    added framework for Intern.test.js file

---
+++ tests/Intern.test.js
@@ -0,0 +1,28 @@
+// create variable to require the Employee.js file and the Intern.test.js file
+const Intern = require("../lib/Intern");
+const Employee = require("../lib/Employee");
+
+describe("Intern class", () => {
+    // test the creation of a new instance of the object - should have added property school
+    it("should do something", () => {
+        
+    });
+
+    // test getSchool method
+    desribe("getSchool", () =>{
+        it("should do something", () => {
+            
+        });
+
+    });
+
+    // test getRole method - should be intern
+    describe("getRole", () => {
+        it("should do something", () => {
+            
+        });
+
+    });
+
+
+})
