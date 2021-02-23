c1a2d1d2f71d4671445976509a0b0c25f9f776d4
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-02-22 21:49:16 -0600

    added packages and class file requirements

---
+++ index.js
@@ -0,0 +1,10 @@
+// required packages
+const inquirer = require("inquirer");
+const jest = require("jest");
+
+// read in class files
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+
