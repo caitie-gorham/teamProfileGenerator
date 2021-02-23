e0a7730c8a21c4c559972ef948a2a64d77d69fe3
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-02-22 20:58:51 -0600

    added framework for engineer.test.js file

---
+++ tests/Engineer.test.js
@@ -0,0 +1,28 @@
+// create variable to require the Employee.js file and the Engineer.js file
+const Engineer = require("../lib/Engineer");
+const Employee = require('../lib/Employee');
+
+describe("Engineer class", () => {
+    // test the creation of a new instance of the object - should have added property github
+    it("should do something", () => {
+        
+    });
+    
+    // test getGithub method
+    desribe("getGithub", () =>{
+        it("should do something", () => {
+            
+        });
+
+    });
+
+    // test getRole method - should be engineer
+    describe("getRole", () => {
+        it("should do something", () => {
+            
+        });
+
+    });
+})
+
+
