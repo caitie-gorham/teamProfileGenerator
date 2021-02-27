fdd5fe57a0016769dffb97477ba03c85ead98366
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-02-26 19:32:56 -0600

    added initial inquirer prompt

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 // required packages
 const inquirer = require("inquirer");
 const jest = require("jest");
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // read in class files
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
+// inquirer prompt to get back workrole of employee and kick off class-specific inquirer prompt
+function kickPrompt() {
+    // return the inquirer prompt
+    return inquirer.prompt([
+        {
+            type: "list",
+            message: "Choose the employee work role",
+            name: "workrole",
+            choice: ["Manager", "Engineer", "Intern"]
+        }
+    ]).then(response => {
+        if(response.choices === "Manager") {
+            kickManager();
+        } else if (response.choices === "Engineer") {
+            kickEngineer();
+        } else 
+            kickIntern();
+    });
+}
+// class-specific inquirer prompts
+function kickManager();
+function kickEngineer();
+function kickIntern();
+
+// function to kick off inquirer prompts
+function init() {
+    kickPrompt();
+}
+
+// call init() function
+init();
 
