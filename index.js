228517551a5408fc056c84b467ec2cb1824773d1
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-02-27 19:54:45 -0600

    added html template literals for main html and for each card to be added

--- index.js
+++ index.js
@@ -167,6 +167,52 @@ function kickIntern() {
     })
 };
 
+const teamProfiles = () => {
+    let cardString = "";
+    for(i = 0; i < fullTeam.length; i++) {
+        cardString += `<div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">${fullTeam[i].name}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">${fullTeam[i].getRole()}</h6>
+          <p class="card-text">${fullTeam[i].id}</p>
+          <p class="card-text">${fullTeam[i].email}</p>
+          <p class="card-text">${fullTeam[i].getValue()}</p>
+        </div>
+      </div>`
+    }
+
+    console.log(cardString);
+
+    return cardString;
+}
+
+function buildPage() {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Builder</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <link href="./style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="page-header bg-danger text-white text-center">
+                <h2>My Team</h2>
+            </div>
+            ${teamProfiles}
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
+            crossorigin="anonymous"></script>
+    </body>
+    </html>`
+}
+
+
 // callback function to kickPrompt()
 function init() {
     kickPrompt();
